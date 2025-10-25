244178b586e20b6f0a9777fce63fb1d6e950e938
Verbatim_T (Tim) <6174906+verbatimt@users.noreply.github.com>
2025-10-24 19:29:36 -0700

    Assembly Updating Info

--- MixItUp.Uninstaller/Properties/AssemblyInfo.cs
+++ MixItUp.Uninstaller/Properties/AssemblyInfo.cs
@@ -3,13 +3,13 @@ using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+// associated with an assembly. 
 [assembly: AssemblyTitle("MixItUp.Uninstaller")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("Mix It Up Desktop Uninstaller")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCompany("Blazing Cacti LLC")]
 [assembly: AssemblyProduct("MixItUp.Uninstaller")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright © 2025 Blazing Cacti LLC, All Rights Reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -37,5 +37,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
