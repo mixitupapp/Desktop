79b9026ca972e3b3ec3dc7dd3c9fce085ad2cdec
🅰️🅰️🅰️1️⃣6️⃣2️⃣ ✔️ <admin@aaa162.live>
2025-11-14 21:23:54 -0600

    Remove API.NET8 and update MixItUp.API from netstandard to .net10

--- APIs/MixItUp.API.NET8/V2/Models/GetSingleCommandResponse.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MixItUp.API.V2.Models
-{
-    public class GetSingleCommandResponse
-    {
-        public Command Command { get; set; }
-    }
-}
--- APIs/MixItUp.API.NET8/V2/Models/GetSingleUserResponse.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MixItUp.API.V2.Models
-{
-    public class GetSingleUserResponse
-    {
-        public User User { get; set; }
-    }
-}
