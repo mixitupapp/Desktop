eaa01a36918a5162c661794ed477e1e6854e1d95
aaa162 <48528696+MrAAA162@users.noreply.github.com>
2025-12-29 19:42:59 -0600

    (feat) Add platform selection for timer commands (#351)

--- MixItUp.Base/Model/Commands/TimerCommandModel.cs
+++ MixItUp.Base/Model/Commands/TimerCommandModel.cs
@@ -6,6 +6,9 @@ namespace MixItUp.Base.Model.Commands
     [DataContract]
     public class TimerCommandModel : CommandModelBase
     {
+        [DataMember]
+        public StreamingPlatformTypeEnum Platform { get; set; } = StreamingPlatformTypeEnum.All;
+
         public TimerCommandModel(string name) : base(name, CommandTypeEnum.Timer) { }
 
         [Obsolete]
