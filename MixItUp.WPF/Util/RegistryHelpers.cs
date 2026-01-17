8dd17d5263ac21ad5b3b7fc2ca44db3b335ac36b
🅰️🅰️🅰️1️⃣6️⃣2️⃣ ✔️ <admin@aaa162.live>
2026-01-17 00:00:55 -0600

    move uninstaller to its own subfolder to avoid conflict with .net8/10 dlls

--- MixItUp.WPF/Util/RegistryHelpers.cs
+++ MixItUp.WPF/Util/RegistryHelpers.cs
@@ -125,7 +125,7 @@ namespace MixItUp.WPF.Util
                     }
 
                     Version v = asm.GetName().Version;
-                    string uninstallerPath = Path.Combine(installDir, "MixItUp.Uninstaller.exe");
+                    string uninstallerPath = Path.Combine(installDir, "Uninstaller", "MixItUp.Uninstaller.exe");
 
                     key.SetValue("DisplayName", "Mix It Up");
                     key.SetValue("ApplicationVersion", v.ToString());
