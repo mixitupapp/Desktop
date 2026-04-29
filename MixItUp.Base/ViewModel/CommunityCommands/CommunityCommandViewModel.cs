31bf7bf6ab38d9c918f37b42698c187af28129fe
🅰️🅰️🅰️1️⃣6️⃣2️⃣ ✔️ <admin@aaa162.live>
2026-04-28 23:50:50 -0500

    Revert "maint: include community command ratings that do not have any reviews"
    This reverts commit 36ab1cfb796fe6449c544051120dab0adb301777.

--- MixItUp.Base/ViewModel/CommunityCommands/CommunityCommandViewModel.cs
+++ MixItUp.Base/ViewModel/CommunityCommands/CommunityCommandViewModel.cs
@@ -81,7 +81,10 @@ namespace MixItUp.Base.ViewModel.CommunityCommands
 
             foreach (CommunityCommandReviewModel review in this.model.Reviews.OrderByDescending(r => r.DateTime))
             {
-                this.Reviews.Add(new CommunityCommandReviewViewModel(review));
+                if (!string.IsNullOrWhiteSpace(review.Review))
+                {
+                    this.Reviews.Add(new CommunityCommandReviewViewModel(review));
+                }
             }
             this.NotifyPropertyChanged("HasNoReviews");
         }
