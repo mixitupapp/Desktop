30b805b981cc1ac64e0b82f50ba73f277466bbd6
Verbatim_T (Tim) <6174906+verbatimt@users.noreply.github.com>
2026-05-27 19:48:01 -0700

    Override bindings for LoadingStatusBar + Adjusted Login Bar

--- MixItUp.WPF/Controls/LoadingStatusBar.xaml.cs
+++ MixItUp.WPF/Controls/LoadingStatusBar.xaml.cs
@@ -8,6 +8,35 @@ namespace MixItUp.WPF.Controls
     /// </summary>
     public partial class LoadingStatusBar : UserControl
     {
+        public static readonly DependencyProperty BarHeightProperty =
+            DependencyProperty.Register(nameof(BarHeight), typeof(double), typeof(LoadingStatusBar), new PropertyMetadata(4.0));
+
+        public double BarHeight
+        {
+            get => (double)GetValue(BarHeightProperty);
+            set => SetValue(BarHeightProperty, value);
+        }
+
+        public static readonly DependencyProperty BarColorProperty =
+            DependencyProperty.Register(nameof(BarColor), typeof(System.Windows.Media.Brush), typeof(LoadingStatusBar),
+                new PropertyMetadata(null, (d, e) => { if (d is LoadingStatusBar b && e.NewValue is System.Windows.Media.Brush br) b.StatusBar.Foreground = br; }));
+
+        public System.Windows.Media.Brush BarColor
+        {
+            get => (System.Windows.Media.Brush)GetValue(BarColorProperty);
+            set => SetValue(BarColorProperty, value);
+        }
+
+        public static readonly DependencyProperty BarBackgroundProperty =
+            DependencyProperty.Register(nameof(BarBackground), typeof(System.Windows.Media.Brush), typeof(LoadingStatusBar),
+                new PropertyMetadata(null, (d, e) => { if (d is LoadingStatusBar b && e.NewValue is System.Windows.Media.Brush br) b.StatusBar.Background = br; }));
+
+        public System.Windows.Media.Brush BarBackground
+        {
+            get => (System.Windows.Media.Brush)GetValue(BarBackgroundProperty);
+            set => SetValue(BarBackgroundProperty, value);
+        }
+
         public LoadingStatusBar()
         {
             InitializeComponent();
