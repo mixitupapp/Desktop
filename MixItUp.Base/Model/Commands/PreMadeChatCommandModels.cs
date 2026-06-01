19ad903fe08295fd9a2e164b063ec4a759582676
🅰️🅰️🅰️1️⃣6️⃣2️⃣ ✔️ <admin@aaa162.live>
2026-06-01 13:08:49 -0500

    fix: Allow !linkaccount to link from all 3 platforms

--- MixItUp.Base/Model/Commands/PreMadeChatCommandModels.cs
+++ MixItUp.Base/Model/Commands/PreMadeChatCommandModels.cs
@@ -987,7 +987,7 @@ namespace MixItUp.Base.Model.Commands
 
     public class LinkAccountPreMadeChatCommandModel : PreMadeChatCommandModelBase
     {
-        public static Dictionary<Guid, Guid> LinkedAccounts = new Dictionary<Guid, Guid>();
+        public static Dictionary<Guid, HashSet<Guid>> LinkedAccounts = new Dictionary<Guid, HashSet<Guid>>();
 
         public LinkAccountPreMadeChatCommandModel() : base("Link Account", "linkaccount", 0, UserRoleEnum.User) { }
 
@@ -1012,15 +1012,17 @@ namespace MixItUp.Base.Model.Commands
                     return;
                 }
 
-                if (LinkedAccounts.ContainsKey(user.ID) && LinkedAccounts[user.ID] == parameters.User.ID)
+                if (LinkedAccounts.TryGetValue(user.ID, out HashSet<Guid> pendingForUser) && pendingForUser.Contains(parameters.User.ID))
                 {
-                    LinkedAccounts.Remove(user.ID);
+                    pendingForUser.Remove(parameters.User.ID);
+                    if (pendingForUser.Count == 0) { LinkedAccounts.Remove(user.ID); }
                     UserV2ViewModel.MergeUserData(user, parameters.User);
                     await ServiceManager.Get<ChatService>().SendMessage(MixItUp.Base.Resources.LinkAccountCommandAccountsLinkedSuccessfully, parameters);
                 }
                 else
                 {
-                    LinkedAccounts[parameters.User.ID] = user.ID;
+                    if (!LinkedAccounts.ContainsKey(parameters.User.ID)) { LinkedAccounts[parameters.User.ID] = new HashSet<Guid>(); }
+                    LinkedAccounts[parameters.User.ID].Add(user.ID);
                     await ServiceManager.Get<ChatService>().SendMessage(string.Format(MixItUp.Base.Resources.LinkAccountCommandPleaseConfirmLink, parameters.Platform, parameters.User.Username), parameters);
                 }
             }
