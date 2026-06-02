7e596a5a44403417b94792a7f42cb65a4276c0a9
verbatimt <6174906+verbatimt@users.noreply.github.com>
2026-06-02 11:21:19 -0700

    Version Bump v1.7.020

--- MixItUp.WPF/Util/BuildExpirationHelper.cs
+++ MixItUp.WPF/Util/BuildExpirationHelper.cs
@@ -8,7 +8,7 @@ namespace MixItUp.WPF.Util
     {
         private static readonly bool ENABLED = false;  // Set to true to enable build expiration checking
 
-        private static readonly DateTime EXPIRATION_DATE = new DateTime(2026, 5, 29); // (year, month, day) - Set expiration date here
+        private static readonly DateTime EXPIRATION_DATE = new DateTime(2026, 6, 2); // (year, month, day) - Set expiration date here
 
         public static bool IsEnabled()
         {
