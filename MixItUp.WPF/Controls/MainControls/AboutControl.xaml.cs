3efd225f85a05b84fed9704fe59a6b0bd70b8708
verbatimt <6174906+verbatimt@users.noreply.github.com>
2026-06-05 10:51:32 -0700

    App/Bot Swap

--- MixItUp.WPF/Controls/MainControls/AboutControl.xaml.cs
+++ MixItUp.WPF/Controls/MainControls/AboutControl.xaml.cs
@@ -141,7 +141,7 @@ namespace MixItUp.WPF.Controls.MainControls
 
         private void WikiButton_Click(object sender, RoutedEventArgs e) { ServiceManager.Get<IProcessService>().LaunchLink("https://wiki.mixitupapp.com/"); }
 
-        private void GithubButton_Click(object sender, RoutedEventArgs e) { ServiceManager.Get<IProcessService>().LaunchLink("https://github.com/mixitupapp/Desktop"); }
+        private void GithubButton_Click(object sender, RoutedEventArgs e) { ServiceManager.Get<IProcessService>().LaunchLink("https://github.com/mixitupbot/Desktop"); }
 
         private void SaviorXTanrenButton_Click(object sender, RoutedEventArgs e) { ServiceManager.Get<IProcessService>().LaunchLink("https://twitch.tv/SaviorXTanren"); }
 
